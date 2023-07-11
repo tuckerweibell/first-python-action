992d43b272e417be25b23e93caabd6aa0e2b3098
tuckerweibell <tucker.weibell@ezcater.com>
2023-07-11 09:02:56 -0600

    test

--- installer.sh
+++ installer.sh
@@ -3,9 +3,6 @@
 URL="$CONSOLE_URL/api/$API_VERSION/util/twistcli"
 
 
-echo $TWISTLOCK_PASSWORD | curl -k \
-    -u $TWISTLOCK_USER \
-    -X GET -o twistcli \
-    "$URL"  
+echo $TWISTLOCK_PASSWORD | curl -k -u $TWISTLOCK_USER -X GET -o twistcli $URL 
 
 if [$? == 0]; then echo "Twistcli Linux Installed Successfully"
